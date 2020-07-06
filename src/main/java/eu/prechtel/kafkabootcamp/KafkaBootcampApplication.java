12c275c06a9815cedb838e2056846c5f6eb57d4d
Frank Prechtel <frank@prechtel.eu>
2020-07-06 20:08:08 +0200

    initial commit

---
+++ src/main/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplication.java
@@ -0,0 +1,13 @@
+package eu.prechtel.kafkabootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBootcampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBootcampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.prechtel.kafkabootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
