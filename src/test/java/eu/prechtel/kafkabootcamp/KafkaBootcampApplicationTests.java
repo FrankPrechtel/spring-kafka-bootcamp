54f7a79256bf7a1da5586acee7ad2e3233b7ad0b
Frank Prechtel <frank@prechtel.eu>
2020-07-12 23:04:39 +0200

    add basic EmbeddedKafkaBroker unit test

--- src/test/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplicationTests.java
+++ src/test/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplicationTests.java
@@ -1,13 +1,21 @@
 package eu.prechtel.kafkabootcamp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka
 class KafkaBootcampApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    // TODO: constructor based injection with exception
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
 
+    @Test
+    void basicCheck() {
+        System.out.println(embeddedKafkaBroker.getBrokersAsString());
+    }
 }
