359d0b2818bff6360ae7a7a6b965279de2f3a611
Frank Prechtel <frank@prechtel.eu>
2020-07-19 16:05:12 +0200

    fix constructor based injection of EmbeddedKafkaBroker

--- src/test/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplicationTests.java
+++ src/test/java/eu/prechtel/kafkabootcamp/KafkaBootcampApplicationTests.java
@@ -2,20 +2,20 @@ package eu.prechtel.kafkabootcamp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 
-@SpringBootTest
 @EmbeddedKafka
 class KafkaBootcampApplicationTests {
 
-    // TODO: constructor based injection with exception
-    @Autowired
-    private EmbeddedKafkaBroker embeddedKafkaBroker;
+    private EmbeddedKafkaBroker broker;
 
-    @Test
+	public KafkaBootcampApplicationTests(@Autowired EmbeddedKafkaBroker broker) {
+		this.broker = broker;
+	}
+
+	@Test
     void basicCheck() {
-        System.out.println(embeddedKafkaBroker.getBrokersAsString());
+        System.out.println(broker.getBrokersAsString());
     }
 }
