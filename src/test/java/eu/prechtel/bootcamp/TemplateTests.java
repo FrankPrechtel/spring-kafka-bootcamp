bc4bd2577ed6c0c92683426902b6b4fe87073bb6
Frank Prechtel <frank@prechtel.eu>
2020-07-21 20:43:04 +0200

    save WIP

--- src/test/java/eu/prechtel/bootcamp/TemplateTests.java
+++ src/test/java/eu/prechtel/bootcamp/TemplateTests.java
@@ -18,26 +18,14 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("kafka-embedded")
 @SpringBootTest
 @DirtiesContext
-//@EmbeddedKafka(partitions = 1, topics = "example-kafka-topic", zookeeperPort = 2182, ports = 9093, count = 1)// bootstrapServersProperty = "spring.kafka.bootstrap-servers")
-@EmbeddedKafka(
-	partitions = 1,
-	topics = "example-kafka-topic",
-	zookeeperPort = 2181,
-	ports = 9092,
-	count = 1,
-	brokerProperties = {
-		"port = 9092",
-		"advertised.host.name = localhost",
-		"advertised.listeners = PLAINTEXT://127.0.0.1:9092",
-		"listeners = PLAINTEXT://127.0.0.1:9092"
-	})// bootstrapServersProperty = "spring.kafka.bootstrap-servers")
+@EmbeddedKafka(partitions = 1, topics = "example-kafka-topic", zookeeperPort = 2181, ports = 9092, count = 1)// bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TemplateTests {
 
 	Logger logger = LoggerFactory.getLogger(TemplateTests.class);
 
-	//@Autowired
-	//private KafkaTemplate<String, String> embeddedKafka;
+	@Autowired
+	private KafkaTemplate<String, String> embeddedKafka;
 
 	@Autowired
 	private HelloController controller;
@@ -45,47 +33,13 @@ public class TemplateTests {
 	@Test
 	void helloSync() throws Exception {
 		logger.info("helloSync()");
-		//for (int i=0; i<100; i++) Thread.sleep(500L);
-		controller.sendSync("sync my test");
-		Thread.sleep(2000L);
-		controller.sendSync("sync my test");
-		//for (int i=0; i<100; i++) Thread.sleep(500L);
+		controller.sendSync("synchrotron");
+		controller.sendSync("synchrotron");
 	}
 
 	@Test
 	void helloAsync() throws Exception {
 		controller.sendAsync("async my test");
-		Thread.sleep(2000L);
+		controller.sendAsync("async my test");
 	}
-
-	@KafkaListener(groupId = "template-tests", topics = "example-kafka-topic", containerFactory = "kafkaListenerContainerFactory")
-	public void consume(String message) {
-		logger.info("MESSAGE: {}", message);
-		System.exit(1);
-	}
-
-
-	public static void waitForAssignment(KafkaMessageListenerContainer<String, String> container, int partitions)
-		throws Exception {
-
-		Logger logger = LoggerFactory.getLogger(TemplateTests.class);
-
-		logger.info(
-			"Waiting for " + container.getContainerProperties().getTopics() + " to connect to " + partitions + " " +
-				"partitions.");
-
-		int n = 0;
-		int count = 0;
-		while (n++ < 600 && count < partitions) {
-			count = 0;
-
-			if (container.getAssignedPartitions() != null) {
-				count = container.getAssignedPartitions().size();
-			}
-			if (count < partitions) {
-				Thread.sleep(100);
-			}
-		}
-	}
-
 }
