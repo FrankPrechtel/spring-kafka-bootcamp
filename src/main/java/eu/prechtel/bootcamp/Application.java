0b9a47b507094e18203b3c6c69290dd7fb9453c4
Frank Prechtel <frank@prechtel.eu>
2021-12-07 21:09:41 +0100

    upgrade to latest Spring Boot 2.6.1

--- src/main/java/eu/prechtel/bootcamp/Application.java
+++ src/main/java/eu/prechtel/bootcamp/Application.java
@@ -8,6 +8,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		// new SpringApplicationBuilder().sources(Application.class).web(WebApplicationType.SERVLET).run(args);
 	}
 }
